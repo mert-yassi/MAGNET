a2374e365658753868ba0d23aca7cdb7048c068e
Mert Yassi <mert_yassi@icloud.com>
2026-06-29 15:32:28 +1000

    Add files via upload

--- TVLA/MaskedBernoulliExp/target/project.c
+++ TVLA/MaskedBernoulliExp/target/project.c
@@ -17,6 +17,9 @@
 #define l 2
 #define rand_uint32() xoshiro_next()
 
+#define SAMPLE_SEED_OFF 8
+#define MASK_SEED_OFF 24
+
 #define BER_RAND mu
 #define BEXP_RAND l * (BER_RAND + 3) + 20
 
@@ -24,44 +27,59 @@ static inline uint32_t rotl(const uint32_t x, int k) {
 	return (x << k) | (x >> (32 - k));
 }
 
-static uint32_t s[4];
+static uint32_t sample_s[4];
+static uint32_t mask_s[4];
 
-void seed_xoshiro(uint8_t* pt){
-    s[0] = ((uint32_t)pt[11] << 24) | ((uint32_t)pt[10] << 16) | ((uint32_t)pt[9] <<  8) | ((uint32_t)pt[8] );
-    s[1] = ((uint32_t)pt[15] << 24) | ((uint32_t)pt[14] << 16) | ((uint32_t)pt[13] <<  8) | ((uint32_t)pt[12] );
-    s[2] = ((uint32_t)pt[11] << 24) | ((uint32_t)pt[10] << 16) | ((uint32_t)pt[9] <<  8) | ((uint32_t)pt[8] );
-    s[3] = ((uint32_t)pt[15] << 24) | ((uint32_t)pt[14] << 16) | ((uint32_t)pt[13] <<  8) | ((uint32_t)pt[12] );
+static void seed_xoshiro_state(uint32_t st[4], const uint8_t *pt) {
+    st[0] = ((uint32_t)pt[3]  << 24) | ((uint32_t)pt[2]  << 16) | ((uint32_t)pt[1]  << 8) | pt[0];
+    st[1] = ((uint32_t)pt[7]  << 24) | ((uint32_t)pt[6]  << 16) | ((uint32_t)pt[5]  << 8) | pt[4];
+    st[2] = ((uint32_t)pt[11] << 24) | ((uint32_t)pt[10] << 16) | ((uint32_t)pt[9]  << 8) | pt[8];
+    st[3] = ((uint32_t)pt[15] << 24) | ((uint32_t)pt[14] << 16) | ((uint32_t)pt[13] << 8) | pt[12];
 }
 
-uint32_t xoshiro_next(void) {
-	const uint32_t result_starstar = rotl(s[0] * 5, 7) * 9;
+static uint32_t xoshiro_next_state(uint32_t st[4]) {
+    const uint32_t result_starstar = rotl(st[0] * 5, 7) * 9;
+    const uint32_t t = st[1] << 9;
 
-	const uint32_t t = s[1] << 9;
+    st[2] ^= st[0];
+    st[3] ^= st[1];
+    st[1] ^= st[2];
+    st[0] ^= st[3];
 
-	s[2] ^= s[0];
-	s[3] ^= s[1];
-	s[1] ^= s[2];
-	s[0] ^= s[3];
+    st[2] ^= t;
+    st[3] = rotl(st[3], 11);
 
-	s[2] ^= t;
-
-	s[3] = rotl(s[3], 11);
-
-	return result_starstar;
+    return result_starstar;
 }
 
-void GenUniRandBit(uint32_t* x) {
-	for(int i = 0; i < NUM_SHARES; ++i) x[i] = rand_uint32() & 1;
+static inline uint32_t sample_rand_uint32(void) {
+    return xoshiro_next_state(sample_s);
 }
 
-void Refresh(uint32_t* x) {
+static inline uint32_t mask_rand_uint32(void) {
+    return xoshiro_next_state(mask_s);
+}
+
+
+void Refresh(uint32_t* x) { // [ISW03]
 	for(int i = 0; i < NUM_SHARES; ++i) {
 		for(int j = i + 1; j < NUM_SHARES; ++j) {
-			uint32_t r = rand_uint32();
+			uint32_t r = mask_rand_uint32();
 			x[i] ^= r;
 			x[j] ^= r;
 		}
 	}
+}
+
+void GenMaskedSampleBit(uint32_t* x) {
+    uint32_t bit = sample_rand_uint32() & 1;
+
+    x[0] = bit;
+    for(int i = 1; i < NUM_SHARES; ++i) {
+        x[i] = 0;
+    }
+
+    Refresh(x);
 }
 
 void bin_rep_arr(uint32_t *b_msk, int n, double *arr) {
@@ -85,9 +103,10 @@ void bin_rep_arr(uint32_t *b_msk, int n, double *arr) {
 extern void MaskedBernoulliExp_m4(uint32_t *b, uint32_t *u, uint32_t *p_exp, uint32_t *one, uint32_t *rs, uint32_t *pool);
 
 uint8_t get_pt(uint8_t* pt, uint8_t len) {
-    seed_xoshiro(pt);
+    seed_xoshiro_state(sample_s, pt + SAMPLE_SEED_OFF);
+    seed_xoshiro_state(mask_s, pt + MASK_SEED_OFF);
+    
     double r = 2 * SIG * SIG;
-
     double Bexp_bias[l];
     for(int i = 0; i < l; ++i) Bexp_bias[i] = exp(-(1 << i) / r);
     
@@ -105,13 +124,12 @@ uint8_t get_pt(uint8_t* pt, uint8_t len) {
 	uint32_t rr[NUM_SHARES] = {0}, zp[NUM_SHARES] = {0}; int j = 0;
     uint32_t rs[NUM_SHARES * l * mu] = {0}, pool[BEXP_RAND] = {0};
 	for(int i = 0; i < l * mu; ++i) {
-	    GenUniRandBit(rr);
-	    Refresh(rr);
-        rs[j] = rr[0]; j++;
-        rs[j] = rr[1]; j++;
+	    GenMaskedSampleBit(rr);
+        rs[j++] = rr[0];
+        rs[j++] = rr[1];
 	}
 	for(int i = 0; i < BEXP_RAND; ++i) {
-        pool[i] = rand_uint32();
+        pool[i] = mask_rand_uint32();
     }
      
     for (volatile int k = 0; k < 1000; k++) {;} // to clean the power trace
@@ -141,7 +159,7 @@ int main(void) {
 
     simpleserial_init();
 
-    simpleserial_addcmd('p', 16,  get_pt);
+    simpleserial_addcmd('p', 40,  get_pt);
         
     while(1)
         simpleserial_get();
